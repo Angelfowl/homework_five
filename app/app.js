35adf133b23238c26607d0046c9b8b0e0c6c5208
Angelfowl <112956907+Angelfowl@users.noreply.github.com>
2022-11-03 19:39:14 -0400

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,139 @@
+var GAMES = [
+    {   
+        gameTitle: "RomancingSaGa",
+        gameImg: "images/romancingSaga.jpg",
+        gameBriefDescription: 
+        "The Fatestones lie scattered across the world and the gods of evil are resurgent once more. Eight heroes set off on their own journeys, as if guided by the hand of fate.",
+        gameDescription:
+        "The original Romancing SaGa -Minstrel Song- included many of the SaGa series trademark elements, such as the Glimmer and Combo mechanics and was considered to be the epitome of the series when it first released. The free scenario system that lets you create your own storyline remains at the core of the game, letting you select one of eight protagonists with completely different origins and backstories, then set off on a unique journey.",
+        gameRelease: "12/1/2022",
+        gameRating: "T",
+    
+    },
+    {   
+        gameTitle: "Dragon Quest Treasures",
+        gameImg: "images/DQtreasure.jpg",
+        gameBriefDescription: 
+        "Erik and his sister Mia live on a Viking longship, dreaming of the day they can explore the world in search of grand treasure",
+        gameDescription:
+        "With the vast world of Draconia to explore and tons of tantalizing treasure to find, Erik, Mia, and their cute companions will need some help. The team soon befriends and recruits a veritable variety of well-mannered monsters. By making use of these monsters' magical visions of hidden treasure and their unique treasure-hunting abilities, our heroes will race over open fields, scale cliffs with a single bound, and glide over gaping chasms to locate loot...",
+        gameRelease: "12/9/2022",
+        gameRating: "E+",
+    },
+    {   
+        gameTitle: "Fatal Frame 4",
+        gameImg: "images/fatalframe4.jpg",
+        gameBriefDescription: 
+        "The story, set on the fictional Rougetsu Island, focuses on Ruka Minazuki, one of a group of girls who was held captive on the island for unknown reasons. Years after their rescue, still suffering from amnesia, Ruka and the two surviving girls return to the island to seek out the truth.",
+        gameDescription:
+        "The story, set on the fictional Rougetsu Island, focuses on Ruka Minazuki, one of a group of girls who was held captive on the island for unknown reasons. Years after their rescue, still suffering from amnesia, Ruka and the two surviving girls return to the island to seek out the truth. The game's title stems from a ritual mask key to the story. The gameplay, as with previous entries in the series, revolves around the main character exploring environments and tackling hostile ghosts using the Camera Obscura.",
+        gameRelease: "2023",
+        gameRating: "M",
+    },
+    {   
+        gameTitle: "Harvestella",
+        gameImg: "images/harvestella.jpg",
+        gameBriefDescription: 
+        "In a vibrant and colourful world, players will tend their crops, befriend the townsfolk, overcome threats, discover the origins of the world and the truth behind the calamity along the way",
+        gameDescription:
+        "In a vibrant and colourful world, players will tend their crops, befriend the townsfolk, overcome threats, discover the origins of the world and the truth behind the calamity along the way. The story begins when the player wakes up in the middle of Quietus, a calamity that comes with each change of season and threatens all life.",
+        gameRelease: "11/4/2022",
+        gameRating: "T",
+    },
+    {   
+        gameTitle: "Kirby's Return to Dreamland",
+        gameImg: "images/kirbyReturn.jpg",
+        gameBriefDescription: 
+        "The tough puff Kirby is back for a 4-player* platforming adventure. Copy enemies’ abilities and use their power to attack with a sword, wield a whip, and float with a parasol.",
+        gameDescription:
+        "You can also laser blast with the new Mecha Copy Ability that makes a series debut! The extra muscle will be handy in finding missing pieces for Magolor’s ship, which crash landed on Planet Popstar. Float and fight along with friends on the same system, where each player can control another Kirby or one of Kirby’s iconic friends like King Dedede, Meta Knight, and Bandana Waddle Dee",
+        gameRelease: "2/24/2023",
+        gameRating: "E+",
+    },
+    {   
+        gameTitle: "Mineko Night Market",
+        gameImg: "images/minekoNight.jpg",
+        gameBriefDescription: 
+        "Mineko’s Night Market is a game that celebrates Japanese culture while introducing a heartwarming story about friendship, tradition, and many, many cats.",
+        gameDescription:
+        "Play as Mineko, a curious girl who has just arrived at her new home on a struggling Japanese-inspired island at the base of Mount Fugu. The superstitious locals on the island worship the Sun Cat, Abe. What was always thought to be a long-told myth has the town puzzled, as sightings of Abe have begun to surface among the residents of the island in recent days.",
+        gameRelease: "2023",
+        gameRating: "E+",
+    },
+    {   
+        gameTitle: "Persona 3 Portable",
+        gameImg: "images/persona3.jpg",
+        gameBriefDescription: 
+        "A teenager who was orphaned as a young child returns to the city of his/her childhood.",
+        gameDescription:
+        "Shortly after transferring to Gekkoukan High School, he/she is attacked by Shadows - creatures that feed on the minds of their victims. The assault awakens his/her Persona, Orpheus, his/her only chance at defeating these creatures of the night. He/She soon discovers that he/she shares this special ability with other students at his/her new school. From them he/she learns of the Dark Hour, a hidden time that exists between one day and the next, swarming with Shadows. He/She joins the Specialized Extracurricular Execution Squad (SEES), and with his/her new friends, confronts this evil threat.",
+        gameRelease: "1/19/2023",
+        gameRating: "M",
+    },
+    {   
+        gameTitle: "Persona 4 Golden",
+        gameImg: "images/persona4.jpg",
+        gameBriefDescription: 
+        "Inaba—a quiet town in rural Japan sets the scene for budding adolescence in Persona 4 Golden.",
+        gameDescription:
+        "A coming of age story that sets the protagonist and his friends on a journey kickstarted by a chain of serial murders. Explore meeting kindred spirits, feelings of belonging, and even confronting the darker sides of one’s self.",
+        gameRelease: "1/19/2023",
+        gameRating: "M",
+    },
+    {   
+        gameTitle: "Persona 5 Royal",
+        gameImg: "images/persona5royal.jpg",
+        gameBriefDescription: 
+        "Forced to transfer to a high school in Tokyo, the protagonist has a strange dream. You truly are a prisoner of fate. In the near future, ruin awaits you. With the goal of “rehabilitation” looming overhead, he must save others from distorted desires by donning the mask of a Phantom Thief.",
+        gameDescription:
+        "Persona is a RPG series set in modern-day Japan that is acclaimed for its narrative style of confronting everyday realities such as forging friendships and romances, alongside supernatural and paranormal themes, including uncovering and solving mysterious rumors and various urban legends.The series typically follows a story that unfolds after a group of teenagers discover their abilities as Persona-users, chronicling the growth of their friendship and inner selves as they confront numerous obstacles. In recent years, the franchise has been adapted to animated TV series, animated films, music concerts, theatrical stages, manga, and continues to expand its influence.",
+        gameRelease: "10/21/2022",
+        gameRating: "M",
+    },
+    {   
+        gameTitle: "Pokemon Scarlet/Violet",
+        gameImg: "images/pokemonScarlet.jpg",
+        gameBriefDescription: 
+        "Learn new details about Sprigatito, Fuecoco, and Quaxly—the three Pokémon from which you'll choose your first partner—along with other wonderful new Pokémon, including the Legendary Pokémon Koraidon and Miraidon!",
+        gameDescription:
+        "Pokémon Scarlet and Pokémon Violet are the first open-world RPGs in the Pokémon series. As you enjoy the hallmarks of the series—meeting, teaming up with, battling, training, and trading Pokémon—you’ll be able to immerse yourself in a newly evolved adventure.",
+        gameRelease: "11/18/2022",
+        gameRating: "E+",
+    },
+];
+
+function loadData(){
+    $.each(GAMES, function(index, game){
+        $("#app").append(`<div id="${index}" class="wrapper">
+        <div class="video-game">
+          <div class="video-header">
+            <img src="${game.gameImg}" alt="${game.gameTitle}" />
+            <h1>${game.gameTitle}</h1>
+          </div>
+          <div class="brief-video-desc">
+            <h3>Brief Game Description</h3>
+            <p>
+              ${game.gameBriefDescription}
+            </p>
+          </div>
+          <div class="long-video-desc">
+            <h3>Game Description</h3>
+            <p>
+              ${game.gameDescription}
+            </p>
+          </div>
+          <div class="rating-release">
+            <h4>Release Date: ${game.gameRelease}</h4>
+            <h4>Rated: ${game.gameRating}</h4>
+          </div>
+        </div>
+      </div>`);
+    });
+}
+
+
+
+
+$(document).ready(function(){
+    loadData();
+})
